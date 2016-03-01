212980d1fbc776a33c37d294afcdef4c7b9b2e44
Jiaqi Yan <littlepretty881203@gmail.com>
2016-03-01 16:43:16 -0600

    infer with window
    Signed-off-by: Jiaqi Yan <littlepretty881203@gmail.com>

--- phase1.py
+++ phase1.py
@@ -19,25 +19,46 @@ def learnModel(filename, n=48, m=50):
 
     """learn parameters for m*n random variables"""
     means = [[0 for _ in range(0, n)] for _ in range(0, m)]
-    devs = [[0 for _ in range(0, n)] for _ in range(0, m)]
+    stdevs = [[0 for _ in range(0, n)] for _ in range(0, m)]
     for i in range(0, m):
         for j in range(0, n):
             row = [day1[i][j], day2[i][j], day3[i][j]]
             means[i][j] = numpy.mean(row)
-            devs[i][j] = pow(numpy.std(row), 2) / (len(row) - 1)
+            stdevs[i][j] = numpy.std(row) / numpy.sqrt(len(row) - 1)
 
     logging.debug(means[:1])
-    logging.debug(devs[:1])
+    logging.debug(stdevs[:1])
+    return means, stdevs
 
 
-def inferenceTest(filename, n=48, m=50):
+def inferenceTest(filename, means, stdevs, n=48, m=50):
     f = open(filename, 'rb')
     reader = csv.reader(f)
     data = numpy.array(list(reader)).astype('float')
-    logging.debug(data)
+    day1 = data[:, 0:n]
+    day2 = data[:, n:n*2]
+
+    percentage = 0.3
+    for i in range(0, n):
+        test_data = day1[:, i]
+        infer_data = [0 for _ in range(0, m)]
+
+        for j in range(0, m):
+            infer_data[j] = numpy.random.normal(means[j][i], stdevs[j][i])
+
+        window_start = int(i * m * percentage) % m
+        window_size = int(m * percentage)
+
+        for k in range(window_start, window_start + window_size):
+            index = k % m
+            infer_data[index] = test_data[index]
+
+        error = numpy.subtract(test_data, infer_data)
+        logging.debug('error \n'+ str(error))
+
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    learnModel('intelTemperatureTrain.csv')
-    inferenceTest('intelTemperatureTest.csv')
+    means, stdevs = learnModel('intelTemperatureTrain.csv')
+    inferenceTest('intelTemperatureTest.csv', means, stdevs)
