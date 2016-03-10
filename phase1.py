d16d93ccc53ea78020559917c8292577e05cf76d
littlepretty <littlepretty881203@gmail.com>
2016-03-09 20:06:20 -0600

    indent logger
    Signed-off-by: littlepretty <littlepretty881203@gmail.com>

--- phase1.py
+++ phase1.py
@@ -3,6 +3,7 @@
 import csv
 import logging as lg
 import numpy as np
+from python_log_indenter import IndentedLoggerAdapter
 
 
 def learnModel(filename, n=48, m=50):
@@ -15,24 +16,20 @@ def learnModel(filename, n=48, m=50):
         day1.append([float(x) for x in row[:n]])
         day2.append([float(x) for x in row[n:n*2]])
         day3.append([float(x) for x in row[n*2:n*3]])
-
     """learn parameters for m*n random variables"""
-    # means = [[0 for _ in range(0, n)] for _ in range(0, m)]
     means = np.zeros((m, n))
     stdevs = np.zeros((m, n))
-    # stdevs = [[0 for _ in range(0, n)] for _ in range(0, m)]
     for i in range(0, m):
         for j in range(0, n):
             row = [day1[i][j], day2[i][j], day3[i][j]]
             means[i][j] = np.mean(row)
             stdevs[i][j] = np.std(row) / np.sqrt(len(row) - 1)
-
-    lg.debug(means[:1])
-    lg.debug(stdevs[:1])
+    log.debug(str(means[:1]))
+    log.debug(str(stdevs[:1]))
     return means, stdevs
 
 
-def windowInferenceError(day, percentage, n=48, m=50):
+def windowInferenceError(day, means, percentage, n=48, m=50):
     error = []
     for i in range(0, n):
         test_data = day[:, i]
@@ -41,12 +38,10 @@ def windowInferenceError(day, percentage, n=48, m=50):
 
         window_start = int(i * m * percentage) % m
         window_size = int(m * percentage)
-
         """replace inferred data with test data for these inside window"""
         for k in range(window_start, window_start + window_size):
             index = k % m
             infer_data[index] = test_data[index]
-
         """absolute error for time i"""
         error_i = np.subtract(test_data, infer_data)
         error_i = np.absolute(error_i)
@@ -54,7 +49,7 @@ def windowInferenceError(day, percentage, n=48, m=50):
     return error
 
 
-def varianceInferenceError(day, percentage, n=48, m=50):
+def varianceInferenceError(day, means, percentage, n=48, m=50):
     error = []
     for i in range(0, n):
         test_data = day[:, i]
@@ -62,7 +57,7 @@ def varianceInferenceError(day, percentage, n=48, m=50):
         if i != 0:
             """find maximum variances' index"""
             last_error = error[-1]
-            lg.debug('last error \n' + str(last_error))
+            log.debug('last error \n' + str(last_error))
             num_var = int(n * percentage)
             max_indices = []
             for _ in range(0, num_var):
@@ -75,7 +70,6 @@ def varianceInferenceError(day, percentage, n=48, m=50):
             """replace most variant data with test data"""
             for index in max_indices:
                 infer_data[index] = test_data[index]
-
         """absolute error for time i"""
         error_i = np.subtract(test_data, infer_data)
         error_i = np.absolute(error_i)
@@ -83,35 +77,54 @@ def varianceInferenceError(day, percentage, n=48, m=50):
     return error
 
 
-def inferenceTest(filename, n=48, m=50):
+def inferenceTest(filename, means, n=48, m=50):
     f = open(filename, 'rb')
     reader = csv.reader(f)
     data = np.array(list(reader)).astype('float')
     day1 = data[:, 0:n]
     day2 = data[:, n:n*2]
-    percentage = 0.3
+    percentage = [0, 0.05, 0.1, 0.25, 0.5]
+    win_avg_errors = []
+    var_avg_errors = []
+    for p in percentage:
+        day1_err = windowInferenceError(day1, means, p)
+        day2_err = windowInferenceError(day2, means, p)
+        total_err = np.add(day1_err, day2_err)
+        win_avg_err = np.sum(total_err) / total_err.size
+        log.info('Window Inference for %.2f budget' % p)
+        log.debug('error matrix \n' + str(total_err))
+        log.add().info('avg error = ' + str(win_avg_err))
+        log.sub()
+        win_avg_errors.append(win_avg_err)
 
-    day1_error = windowInferenceError(day1, percentage)
-    day2_error = windowInferenceError(day2, percentage)
-    total_error = np.add(day1_error, day2_error)
-    avg_error = np.sum(total_error) / total_error.size
-    lg.info('Window Inference')
-    lg.debug('error matrix \n' + str(total_error))
-    lg.info('avg error = ' + str(avg_error))
+        day1_err = varianceInferenceError(day1, means, p)
+        day2_err = varianceInferenceError(day2, means, p)
+        total_err = np.add(day1_err, day2_err)
+        var_avg_err = np.sum(total_err) / total_err.size
+        log.info('Variance Inference for %.2f budget' % p)
+        log.debug('error matrix \n' + str(total_err))
+        log.add().info('avg error = ' + str(var_avg_err))
+        log.sub()
+        var_avg_errors.append(win_avg_err)
 
-    day1_error = varianceInferenceError(day1, percentage)
-    day2_error = varianceInferenceError(day2, percentage)
-    total_error = np.add(day1_error, day2_error)
-    avg_error = np.sum(total_error) / total_error.size
-    lg.info('Variance Inference')
-    lg.debug('error matrix \n' + str(total_error))
-    lg.info('avg error = ' + str(avg_error))
+    return [win_avg_errors, var_avg_errors]
 
-    return avg_error
+
+def main(train_file, test_file):
+    means, stdevs = learnModel(train_file)
+    (win_avg_err, var_avg_err) = inferenceTest(test_file, means)
 
 
 if __name__ == '__main__':
     # lg.basicConfig(level=lg.DEBUG)
     lg.basicConfig(level=lg.INFO)
-    means, stdevs = learnModel('intelTemperatureTrain.csv')
-    inferenceTest('intelTemperatureTest.csv')
+    log = IndentedLoggerAdapter(lg.getLogger(__name__))
+    log.info('Processing Temperature')
+    log.add()
+    main('intelTemperatureTrain.csv', 'intelTemperatureTest.csv')
+    log.sub()
+
+    log.info('Processing Humidity')
+    log.add()
+    main('intelHumidityTrain.csv', 'intelHumidityTest.csv')
+    log.sub()
